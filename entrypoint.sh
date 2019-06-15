13560e5c00c94379b33c147411dbb6fc966a701d
hoto17296 <mail@hoto.me>
2019-06-15 17:08:42 +0900

    rm unnecessary option

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,7 @@ case "$1" in
   cron)
     test -n "${SCHEDULE}" || abort '$SCHEDULE is not defined.'
     CRON_DIR='/etc/crontabs'
-    FD='/proc/$(cat /var/run/crond.pid)/fd'
-    echo "${SCHEDULE} sh /sync.sh 1> ${FD}/1 2> ${FD}/2" > ${CRON_DIR}/root
+    echo "${SCHEDULE} sh /sync.sh" > ${CRON_DIR}/root
     crond -f -d ${CROND_LOG_LEVEL:-8} -c ${CRON_DIR}
     ;;
 
